8ab61a8e3b8607bc1fd01bdfd3f1da5742d23431
Victor Wei <76926228+victor11235@users.noreply.github.com>
2023-04-06 19:47:51 -0400

    Add files via upload

---
+++ ShadowTomographyTrainer_sum.py
@@ -0,0 +1,160 @@
+import os
+import sys
+path = '/Users/victorwei/Research projects/VQE_2022_summer/NEM_SU2/code'
+sys.path.append(path)
+
+from typing import List, Union, Optional
+import torch
+import utils
+
+#Typing imports
+from exact_solvers import GenericExactState
+from data_utils import MeasurementsInCBDataset
+from nqs_models import NeuralQuantumState
+from torch.optim import Optimizer
+from torch.utils.tensorboard import SummaryWriter
+
+import qiskit
+
+# import helper functions
+import Helper_fun as helper
+import random
+
+
+class ShadowTomographyTrainer():
+    '''
+    The neural quantum state tomography algorithm using classical-shadow based cost function
+    '''
+    
+    def __init__(self,
+                 nqs_model: NeuralQuantumState,
+                 #cliffords,
+                 #results,
+                 circuit,
+                 optimizer: Optimizer,
+                 batch_size:int,
+                 logging_callbacks:List,
+                 eval_callbacks:List,
+                 log_every:int,
+                 eval_every:int,
+                 target_state: Optional[GenericExactState]=None,
+                 max_iters:int=100,
+                 num_samples:int=10000,
+                 K:int=1):
+        '''
+
+        '''
+        #self.cliffords = cliffords
+        #self.results = results
+        self.nqs = nqs_model
+        self.batch_size = batch_size
+        self.optimizer = optimizer
+        self.target_state = target_state
+        self.logging_callbacks = logging_callbacks
+        self.eval_callbacks = eval_callbacks
+        self.max_iters = int(max_iters)
+        self.log_every = log_every
+        self.eval_every = eval_every
+        self.num_qubits = circuit.num_qubits
+        self.num_samples = num_samples
+        self.K = K
+        self.circuit = circuit
+
+
+    def _step(self, i=0):
+        '''
+        Training update for one epoch of neural quantum state tomography. Updates the
+        neural quantum state parameters based on the shadow-tomography based infidelity estimate.
+
+        ''' 
+        with torch.no_grad():
+            # first we collect samples from neural network state
+            NN_samples = self.nqs.sample(self.num_samples)
+            unique_samples, counts = torch.unique(NN_samples, dim=0, return_counts=True)
+            weights = counts.type(torch.double)/self.num_samples
+            # create the list for calculating the fidelity later
+            prediction_list = []
+            
+        random.seed()
+        cliffords_batch, results_batch = helper.random_Clifford_shadow(self.circuit, num_shadow=self.batch_size)
+        
+        loss_vmc = 0
+        for cliff, res in zip(cliffords_batch, results_batch):
+            
+            with torch.no_grad():
+                mat  = cliff.adjoint().to_matrix()
+                for bit,count in res.items():
+                    Ub = mat[:,int(bit,2)] # this is Udag|b>
+                    Clifford_sample_log_amplitudes, weights_new, unique_samples_new = \
+                        helper.samples_to_log_clifford_amplitudes(Ub, unique_samples, weights)                         
+                    NN_sample_log_amplitudes_no_grad = self.nqs.amplitudes(unique_samples_new, return_polar = True)
+                    amp_fraction = (utils.Complex(Clifford_sample_log_amplitudes) - NN_sample_log_amplitudes_no_grad).exp()
+                    
+                    # get the conjugate version of amp_fraction
+                    amp_fraction_conj = amp_fraction.conjugate()
+                    mean = (utils.Complex(weights_new) * amp_fraction).sum(dim=0)
+                    overlap = utils.Complex.abs(mean)**2
+                    
+                    # append to the list for fidelity estimate
+                    overlap_scalar = overlap.detach().numpy()
+                    prediction_i = (2**self.num_qubits+1) * overlap_scalar - 1 
+                    prediction_list.append(prediction_i)
+            # calculate the gradient 
+            
+            # get real and imgaginary parts of term 1
+            log_amplitudes = self.nqs.amplitudes(unique_samples_new, return_polar=True)
+            term_1_log_moduli = amp_fraction_conj.real * log_amplitudes.real
+            term_1_phases = amp_fraction_conj.imag * log_amplitudes.imag
+            term_1_real = (weights_new*(term_1_log_moduli - term_1_phases)).sum(dim=0)       
+            term_1_imag = (weights_new*(amp_fraction_conj.real * log_amplitudes.imag + amp_fraction_conj.imag * 
+                                        log_amplitudes.real)).sum(dim=0) 
+            
+            # now since loss = term_1 * term_2, get the real part of the product
+            term_2_real = mean.real
+            term_2_imag = mean.imag
+            loss_real = term_1_real * term_2_real - term_1_imag * term_2_imag
+            # propagate the gradient for every shadow, here we accumulate the gradient for every shadow
+            loss_vmc_i = - 2 * (2**self.num_qubits+1) * loss_real / self.batch_size
+            loss_vmc += loss_vmc_i
+        
+        # end of the step, we update the model parameters
+        #torch.nn.utils.clip_grad_norm_(self.nqs.parameters(), 2)
+        loss_vmc.backward()
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+        
+        with torch.no_grad():
+            cost_fun = 1 - helper.median_of_mean(prediction_list,self.K)
+            print('Iteration ' + str(i) )
+            print('Cost function is ' + str(cost_fun) )
+        
+        logging_dict = {
+            'iter': i,
+            #'samples': samples.cpu().numpy(),
+            #'logamplitudes_r': log_amplitudes.real.detach().cpu().numpy(),
+            'cost_fun': cost_fun,
+            #'loss_log_moduli': loss_log_moduli.detach().cpu().numpy(),           
+            #'loss_phases': loss_phases.detach().cpu().numpy()
+            }
+
+        return logging_dict, cost_fun
+
+            
+    def train(self):
+        infidelity_list = []
+        cost_fun_list = []
+        for i in range(self.max_iters):
+            logging_dict, cost_fun = self._step(i=i)           
+            infidelity = 1-self.target_state.fidelity_to(self.nqs.full_state())
+            
+            infidelity_list.append(infidelity.detach().numpy())
+            cost_fun_list.append(cost_fun)
+            print('Exact infidelity is ' + str(infidelity.detach().numpy() ) )
+            if (i + 1) % self.log_every == 0:
+                for logging_callback in self.logging_callbacks:
+                    logging_callback(logging_dict)
+            if (i + 1) % self.eval_every == 0:
+                with torch.no_grad():
+                    for eval_callback in self.eval_callbacks:
+                        eval_callback(self.nqs, i)
+        return infidelity_list, cost_fun_list
